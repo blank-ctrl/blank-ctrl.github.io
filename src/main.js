12ed19557373820c7c1c8a513cc9b06a375075ac
blank-ctrl <blanklikemybank@gmail.com>
2024-01-03 15:25:54 +0100

    added broken dropdown

---
+++ src/main.js
@@ -0,0 +1,10 @@
+//let header = document.getElementById('hacker-header');
+//let how = document.getElementById('how');
+
+//header.addEventListener('click', () => {
+//  window.scrollTo({
+//   top: how.getBoundingClientRect().y,
+//   left: how.getBoundingClientRect().x,
+//    behavior: 'smooth'
+//  });
+//});
